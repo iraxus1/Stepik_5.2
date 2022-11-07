623beace081c4d1e25a1ccc3d5c5f1a8c3d7a263
iraxus1 <piotrwiktor31@interia.pl>
2022-11-07 20:51:36 +0100

    update

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.example;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
